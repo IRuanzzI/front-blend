a73f7df9dc5e7ffe19b5688b0a9ee86af21e10d7
Ruan Pablo <rp824194@gmail.com>
2024-09-09 23:12:04 -0300

    f

--- front-bebidas/src/componentes/Container/index.js
+++
@@ -1,9 +0,0 @@
-// src/components/Container.js
-import React from 'react';
-import { ContentContainer } from './style';
-
-const Container = () => {
-  return <ContentContainer />;
-};
-
-export default Container;
